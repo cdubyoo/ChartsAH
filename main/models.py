984617a410dc94a77e2ef3c5332263314d82fe00
cdubyoo <chungwongg@gmail.com>
2020-08-30 13:53:04 -0700

    built out user system

---
+++ main/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from datetime import datetime
+# Create your models here.
+class Post(models.Model):
+    post_title = models.CharField(max_length=200)
+    post_content = models.TextField()
+    post_published = models.DateTimeField('date published', default=datetime.now)
+
+    def __str__(self):
+        return self.post_title
