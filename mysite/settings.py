3dc15bc64481d775ba50277013e511bcb0b73b6b
cdubyoo <chungwongg@gmail.com>
2020-09-22 09:28:39 -0700

    updated ev

--- mysite/settings.py
+++ mysite/settings.py
@@ -38,8 +38,8 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
-DEBUG = True
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
+# DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', 'chartsah.herokuapp.com']
 
@@ -156,7 +156,7 @@ django_heroku.settings(locals())
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'chartsah@gmail.com'
-EMAIL_HOST_PASSWORD = 'hjxtqxgmvgwzgbix'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
